3954f9be59e3fe13cdc50ce82ef1e8feaabcef9e
Aryan Vyawahare <aniketvyawahare891@gmail.com>
2022-11-12 03:16:33 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Sitecardpage from "./components/Site Card/Sitecardpage";
 import Home from "./pages/Home";
 
 function App() {
-  return <Home />;
+  return (
+    <>
+ 
+ <BrowserRouter>
+  
+  <Routes>
+  <Route exact path="/" element={<Home/>}/>
+    <Route exact path="/sites" element={<Sitecardpage/>}/>
+  </Routes>
+
+ </BrowserRouter>
+</>
+  );
 }
 
 export default App;
---
+++ src/components/Site Card/sites2.jsx
@@ -0,0 +1,63 @@
+import courseImg1 from "../../assests/images/img1.jpg";
+import courseImg2 from "../../assests/images/img1.jpg";
+import courseImg3 from "../../assests/images/img1.jpg";
+import "./Sites.css";
+import SiteCard from "./SiteCard";
+const coursesData = [
+    {
+      id: "01",
+      title: "The India Gate is a war memories",
+      // lesson: 12,
+      students: 12.5,
+      rating: 5.9,
+      imgUrl: courseImg1,
+    },
+    {
+        id: "01",
+        title: "The India Gate is a war memories",
+        // lesson: 12,
+        students: 12.5,
+        rating: 5.9,
+        imgUrl: courseImg1,
+      },
+      {
+        id: "01",
+        title: "The India Gate is a war memories",
+        // lesson: 12,
+        students: 12.5,
+        rating: 5.9,
+        imgUrl: courseImg1,
+      },
+      {
+        id: "01",
+        title: "The India Gate is a war memories",
+        // lesson: 12,
+        students: 12.5,
+        rating: 5.9,
+        imgUrl: courseImg1,
+      },
+      {
+        id: "01",
+        title: "The India Gate is a war memories",
+        // lesson: 12,
+        students: 12.5,
+        rating: 5.9,
+        imgUrl: courseImg1,
+      },
+      {
+        id: "01",
+        title: "The India Gate is a war memories",
+        // lesson: 12,
+        students: 12.5,
+        rating: 5.9,
+        imgUrl: courseImg1,
+      },
+      {
+        id: "01",
+        title: "The India Gate is a war memories",
+        // lesson: 12,
+        students: 12.5,
+        rating: 5.9,
+        imgUrl: courseImg1,
+      },
+];
